17a57f32874777d44804aad1d2fe6ac3f432fe5c
Maxime Lévêque <maxlev.oclock@gmail.com>
2024-05-02 10:28:31 +0200

    update images mail

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -49,12 +49,12 @@ export const sendVerificationMail = async (token: string, profile: Profile) => {
     attachments: [
       {
         filename: "logo.png",
-        path: path.join(__dirname, "../mail/logo.png"),
+        path: path.join(__dirname, "../mail/images/logo.png"),
         cid: "logo",
       },
       {
         filename: "welcome.png",
-        path: path.join(__dirname, "../mail/welcome.png"),
+        path: path.join(__dirname, "../mail/images/welcome.png"),
         cid: "welcome",
       },
     ],
@@ -88,12 +88,12 @@ export const sendForgetPasswordLink = async (options: Options) => {
     attachments: [
       {
         filename: "logo.png",
-        path: path.join(__dirname, "../mail/logo.png"),
+        path: path.join(__dirname, "../mail/images/logo.png"),
         cid: "logo",
       },
       {
         filename: "forget_password.png",
-        path: path.join(__dirname, "../mail/forget_password.png"),
+        path: path.join(__dirname, "../mail/images/forget_password.png"),
         cid: "forget_password",
       },
     ],
@@ -123,12 +123,12 @@ export const sendPasswordResetSuccessEmail = async (
     attachments: [
       {
         filename: "logo.png",
-        path: path.join(__dirname, "../mail/logo.png"),
+        path: path.join(__dirname, "../mail/images/logo.png"),
         cid: "logo",
       },
       {
         filename: "forget_password.png",
-        path: path.join(__dirname, "../mail/forget_password.png"),
+        path: path.join(__dirname, "../mail/images/forget_password.png"),
         cid: "forget_password",
       },
     ],
